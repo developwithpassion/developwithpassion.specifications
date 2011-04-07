12a1fcc052e6a63ef5f43778e4678960676a2b88
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-04-07 06:43:57 -0400

    Added an example documenting how to do explicit SUT creation

---
+++ source/developwithpassion.specifications.examples/explicitly_creating_the_system_under_test.cs
@@ -0,0 +1,39 @@
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+
+namespace developwithpassion.specifications.examples
+{
+    public class explicitly_creating_the_system_under_test
+    {
+        [Subject(typeof(Calculator))]
+        public class when_adding_two_numbers : Observes<Calculator>
+        {
+            Establish c = () =>
+            {
+                //the sut_factory has a method that allows you to specify explicit
+                //sut creation
+                sut_factory.create_using(() => new Calculator(4, 5));
+            };
+
+            Because b = () =>
+                result = sut.add(1, 3);
+
+            It should_return_the_sum = () =>
+                result.ShouldEqual(4);
+
+            static int result;
+        }
+    }
+
+    public class Calculator
+    {
+        public Calculator(int some_random_value, int some_other_random_value)
+        {
+        }
+
+        public int add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
