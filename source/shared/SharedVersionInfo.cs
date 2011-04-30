b5898bb8ee8e23b5b469b5902a8052e5cb39277c
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-04-29 20:27:02 -0400

    Updated to version 0.4.0.6

--- source/shared/SharedVersionInfo.cs
+++ source/shared/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.4.0.5")]
-[assembly: AssemblyFileVersion("0.4.0.5")]
-[assembly: AssemblyInformationalVersion("0.4.0.5")]
+[assembly: AssemblyVersion("0.4.0.6")]
+[assembly: AssemblyFileVersion("0.4.0.6")]
+[assembly: AssemblyInformationalVersion("0.4.0.6")]
