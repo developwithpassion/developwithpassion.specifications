d055cf698f7e6d1bf27d18d6bb830aef6ae8bcce
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-10-09 12:32:47 -0600

    Updated

--- source/shared/SharedVersionInfo.cs
+++ source/shared/SharedVersionInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.5.1")]
-[assembly: AssemblyFileVersion("0.5.1")]
-[assembly: AssemblyInformationalVersion("0.5.1")]
+[assembly: AssemblyVersion("0.5.9")]
+[assembly: AssemblyFileVersion("0.5.9")]
+[assembly: AssemblyInformationalVersion("0.5.9")]
