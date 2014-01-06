9f00a34f6358e042fde04e4ce3bd5c99188eaa5a
jeffbarnes <jeff@jeffbarnes.net>
2014-01-06 11:25:38 -0600

    Updated all third party references. Made necessary changes to work with separate libaries in mspec. Updated hard-coded reference in build scripts to use new version of mspec in packages

--- build/tasks/configuration.rb
+++ build/tasks/configuration.rb
@@ -15,7 +15,7 @@ task :configure do
     :specs =>
     {
       :runner_options => ["-x","example"],
-      :tools_folder => delayed{File.join("packages","Machine.Specifications.0.5.9","tools")},
+      :tools_folder => delayed{File.join("packages","Machine.Specifications.0.6.1","tools")},
       :dir => delayed{configatron.artifacts_dir.join('specs')},
       :report_dir => delayed{configatron.specs.dir.join('report')},
       :assemblies => dynamic{Dir.glob("#{configatron.artifacts_dir}/*specs.dll")},
