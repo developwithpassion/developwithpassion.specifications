b04ede897bbd7a33ca1fa74a6fb6f0aff808c76e
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2016-09-13 11:17:39 -0600

    Remove generated files

--- source/shared/SharedAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyCompany("Develop With Passion®")]
-[assembly: AssemblyProduct("developwithpassion.specifications")]
-[assembly: AssemblyCopyright("Copyright © Develop With Passion® 2010, 2011")]
-[assembly: AssemblyTrademark("")]
